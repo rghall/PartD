241cc31478e28566e77b77746431319d85925059
rghall <71434382+rghall@users.noreply.github.com>
2020-09-17 21:56:37 +1200

    Update PartD.java

--- src/PartD.java
+++ src/PartD.java
@@ -4,6 +4,7 @@ public class PartD {
 		System.out.println("Hi, my name is Robert");
 		System.out.println("My favourite colour is Red");
 		System.out.println("My favourite mode of transport is driving");
+		System.out.println("The best day of the week is Monday");
 	}
 
 }
