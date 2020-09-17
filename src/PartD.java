24108565ad36fa55d258a3a05759d122673e9b97
rghall <71434382+rghall@users.noreply.github.com>
2020-09-17 21:56:04 +1200

    Update PartD.java

--- src/PartD.java
+++ src/PartD.java
@@ -3,6 +3,7 @@ public class PartD {
 	public static void main(String[] args) {
 		System.out.println("Hi, my name is Robert");
 		System.out.println("My favourite colour is Red");
+		System.out.println("My favourite mode of transport is driving");
 	}
 
 }
